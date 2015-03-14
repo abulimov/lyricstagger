6492925bb34119f91852647f40b2f23cebf932c0
Alexander Bulimov <lazywolf0@gmail.com>
2015-03-14 13:11:38 +0300

    ADD summary line after action

--- lyricstagger/console.py
+++ lyricstagger/console.py
@@ -31,8 +31,9 @@ except ImportError:
 
 def main():
     """Main function"""
-    arguments = docopt(__doc__, version="lyricstagger 0.6.2")
+    arguments = docopt(__doc__, version="lyricstagger 0.7.0")
     path_list = arguments["<path>"]
+    logger = log.cli_logger()
     for i, path in enumerate(path_list):
         try:
             path_list[i] = path.decode("utf-8")
@@ -41,51 +42,62 @@ def main():
 
     if arguments['tag']:
         for filepath in misc.get_file_list(path_list):
-            log.debug("processing file %s", filepath)
+            logger.log_processing(filepath)
             audio = misc.get_audio(filepath)
             data = misc.get_tags(audio)
-            if data and not "lyrics" in data:
+            if data and "lyrics" not in data:
                 lyrics = misc.fetch(data["artist"],
                                     data["title"],
                                     data["album"])
                 if lyrics:
-                    log.debug("writing LYRICS tag to file '%s'", filepath)
+                    logger.log_writing(filepath)
                     audio = misc.write_lyrics(audio, lyrics)
                     audio.save()
                 else:
-                    log.debug("no lyrics found for file '%s'", filepath)
+                    logger.log_not_found(filepath)
 
     elif arguments['remove']:
         for filepath in misc.get_file_list(path_list):
+            logger.log_processing(filepath)
             audio = misc.get_audio(filepath)
+            logger.log_removing(filepath)
             misc.remove_lyrics(audio)
             audio.save()
 
     elif arguments['edit']:
         for filepath in misc.get_file_list(path_list):
+            logger.log_processing(filepath)
             audio = misc.get_audio(filepath)
             lyrics = misc.edit_lyrics(audio)
             if lyrics:
+                logger.log_writing(filepath)
                 audio = misc.write_lyrics(audio, lyrics)
                 audio.save()
             else:
-                log.debug("no lyrics saved for edited file '%s'", filepath)
+                logger.log_no_lyrics_saved(filepath)
 
     elif arguments['show']:
         for filepath in misc.get_file_list(path_list):
+            logger.log_processing(filepath)
             audio = misc.get_audio(filepath)
             data = misc.get_tags(audio)
             if data and "lyrics" in data:
-                print("Artist: %s, Title: %s" % (data["artist"], data["title"]))
+                print("Artist: %s, Title: %s" % (data["artist"],
+                                                 data["title"]))
                 print()
                 print(data["lyrics"])
                 print()
             else:
+                logger.log_not_found(filepath)
                 print("No lyrics in file '%s'" % filepath)
 
     else:  # report
         for filepath in misc.get_file_list(path_list):
+            logger.log_processing(filepath)
             audio = misc.get_audio(filepath)
             data = misc.get_tags(audio)
-            if data and not 'lyrics' in data:
+            if data and 'lyrics' not in data:
+                logger.log_not_found(filepath)
                 print("No lyrics in file '%s'" % filepath)
+
+    print(logger.show_stats())
--- lyricstagger/log.py
+++ lyricstagger/log.py
@@ -2,7 +2,7 @@
 
 import logging
 import os
-#set up logging
+# set up logging
 if 'DEBUG' in os.environ:
     LOGGING_LEVEL = logging.DEBUG
 else:
@@ -10,6 +10,39 @@ else:
 
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]#\
         %(levelname)-8s [%(asctime)s]  %(message)s', level=LOGGING_LEVEL)
+
+
+class cli_logger:
+    def __init__(self):
+        self.stats = dict()
+        for key in ["processed", "written", "removed", "not_found", "not_saved"]:
+            self.stats[key] = 0
+
+    def log_processing(self, filepath):
+        logging.debug("processing audio file '%s'", filepath)
+        self.stats["processed"] += 1
+
+    def log_writing(self, filepath):
+        logging.debug("writing LYRICS tag to file '%s'", filepath)
+        self.stats["written"] += 1
+
+    def log_removing(self, filepath):
+        logging.debug("removing LYRICS tag from file '%s'", filepath)
+        self.stats["removed"] += 1
+
+    def log_no_lyrics_saved(self, filepath):
+        logging.debug("no lyrics saved for edited file '%s'", filepath)
+        self.stats["not_saved"] += 1
+
+    def log_not_found(self, filepath):
+        logging.debug("no lyrics found for file '%s'", filepath)
+        self.stats["not_found"] += 1
+
+    def show_stats(self):
+        report = "Summary -"
+        for key in self.stats:
+            report += (" %s: %d" % (key, self.stats[key]))
+        return(report)
 
 debug = logging.debug
 
